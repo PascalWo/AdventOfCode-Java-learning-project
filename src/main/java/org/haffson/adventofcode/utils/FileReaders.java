05e06e4701e0a140fd76256d021b4bc39c8cc650
PascalWo <pwolff.de@gmail.com>
2022-07-01 16:57:05 +0200

    implemented file reader and transform input file to int array

--- src/main/java/org/haffson/adventofcode/utils/FileReaders.java
+++ src/main/java/org/haffson/adventofcode/utils/FileReaders.java
@@ -1,12 +1,11 @@
 package org.haffson.adventofcode.utils;
 
+import org.haffson.adventofcode.days.day01.MyFileNotFoundException;
 import org.springframework.stereotype.Component;
 
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
 
 /**
  * File readers to load and parse input files for the puzzles.
@@ -14,4 +13,17 @@ import java.util.Scanner;
 @Component
 public class FileReaders {
 
+    public int[] getInputArray(String fileName) {
+
+        try(Scanner scanner = new Scanner(new File(String.valueOf(fileName)))) {
+            int[] inputIntArray = new int[1028];
+            int i = 0;
+            while (scanner.hasNextInt()) {
+                inputIntArray[i++] = scanner.nextInt();
+            }
+            return inputIntArray;
+        } catch (FileNotFoundException e) {
+            throw new MyFileNotFoundException("FileInput not found", e);
+        }
+    }
 }
