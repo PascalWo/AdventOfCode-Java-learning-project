5137880d540812cab3ac37f53c9d506cc1bde96d
PascalWo <pwolff.de@gmail.com>
2022-07-06 14:07:35 +0200

    test day3 part2

--- src/test/java/org/haffson/adventofcode/days/day03/Day03Test.java
+++ src/test/java/org/haffson/adventofcode/days/day03/Day03Test.java
@@ -62,12 +62,33 @@ public class Day03Test {
         );
 
         final List<RectangleClaim> expectedResult = Arrays.asList(
-                new RectangleClaim("1", 1, 3, 4, 4),
-                new RectangleClaim("2", 3, 1, 4, 4),
-                new RectangleClaim("3", 5, 5, 2, 2));
+                new RectangleClaim(1, 1, 3, 4, 4),
+                new RectangleClaim(2, 3, 1, 4, 4),
+                new RectangleClaim(3, 5, 5, 2, 2));
 
         //act
         final List<RectangleClaim> actualResult = day03.convertStringToRectangleList(stringList);
+
+        //assert
+        Assert.assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void test_secondPart_returnsExpectedResult() {
+        //arrange
+        final Day03 day03 = new Day03(fileReaders);
+        when(fileReaders.getInputList("src/main/resources/puzzle_input/day3_input.txt"))
+                .thenReturn(new ArrayList<>(Arrays.asList(
+                        "#1 @ 1,3: 4x4",
+                        "#2 @ 3,1: 4x4",
+                        "#3 @ 5,5: 2x2",
+                        "#5 @ 3,2: 3x3"
+                )));
+
+        final String expectedResult = "Part 2 - not overlapping claim: " + 3;
+
+        //act
+        final String actualResult = day03.secondPart();
 
         //assert
         Assert.assertEquals(expectedResult, actualResult);
