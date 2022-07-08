795f5016dd2f9c70eef634ae4f07172ef3fc730f
PascalWo <102019599+PascalWo@users.noreply.github.com>
2022-07-08 11:28:21 +0200

    Update src/main/java/org/haffson/adventofcode/days/day03/Day03.java
    Co-authored-by: Michelle Fernandez Bieber <37021266+mfbieber@users.noreply.github.com>

--- src/main/java/org/haffson/adventofcode/days/day03/Day03.java
+++ src/main/java/org/haffson/adventofcode/days/day03/Day03.java
@@ -107,8 +107,7 @@ public class Day03 implements Days {
 
         claimList.forEach(claim ->
                 IntStream.range(claim.getLeftSpace(), claim.getRightXCoordinate())
-                        .forEach(i
-                                -> IntStream.range(claim.getTopSpace(), claim.getBottomYCoordinate())
+                        .forEach(i -> IntStream.range(claim.getTopSpace(), claim.getBottomYCoordinate())
                                 .forEach(j
                                         -> fabric[i][j] = fabric[i][j] == null ? claim.getId() : "X")));
         return fabric;
