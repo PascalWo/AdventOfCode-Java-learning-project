a9a00c9abef0e0ca65f56a86b4465361e42a1637
PascalWo <pwolff.de@gmail.com>
2022-07-11 10:17:10 +0200

    fixed class to get correct TimeStampInformation class of string input

--- src/main/java/org/haffson/adventofcode/days/day04/TimeStampInformation.java
+++ src/main/java/org/haffson/adventofcode/days/day04/TimeStampInformation.java
@@ -1,58 +1,37 @@
 package org.haffson.adventofcode.days.day04;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Objects;
+import java.util.regex.Pattern;
 
 public class TimeStampInformation {
-    private final int year;
-    private final int month;
-    private final int day;
-    private final int hour;
-    private final int minute;
-    private final String action;
+    private final LocalDateTime timeStamp;
+    private final String information;
 
-    public TimeStampInformation(final int year, final int month, final int day, final int hour, final int minute, final String action) {
-        this.year = year;
-        this.month = month;
-        this.day = day;
-        this.hour = hour;
-        this.minute = minute;
-        this.action = action;
+    public TimeStampInformation(final LocalDateTime timeStamp, final String information) {
+        this.timeStamp = timeStamp;
+        this.information = information;
     }
 
-    public static TimeStampInformation of(final String input){
+    public static TimeStampInformation of(final String input) {
 
-        final int year = 0;
-        final int month = 0;
-        final int day = 0;
-        final int hour = 0;
-        final int minute = 0;
-        final String action = null;
+        final String stringDate = input.split(Pattern.quote("["))[1].split(Pattern.quote("]"))[0];
 
-        return new TimeStampInformation(year, month, day, hour, minute, action);
+        final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+
+        final LocalDateTime date = LocalDateTime.parse(stringDate, dateTimeFormatter);
+        final String information = input.split("] ")[1];
+
+        return new TimeStampInformation(date, information);
     }
 
-    public int getYear() {
-        return year;
+    public LocalDateTime getTimeStamp() {
+        return timeStamp;
     }
 
-    public int getMonth() {
-        return month;
-    }
-
-    public int getDay() {
-        return day;
-    }
-
-    public int getHour() {
-        return hour;
-    }
-
-    public int getMinute() {
-        return minute;
-    }
-
-    public String getAction() {
-        return action;
+    public String getInformation() {
+        return information;
     }
 
     @Override
@@ -60,23 +39,19 @@ public class TimeStampInformation {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         final TimeStampInformation that = (TimeStampInformation) o;
-        return year == that.year && month == that.month && day == that.day && hour == that.hour && minute == that.minute && Objects.equals(action, that.action);
+        return Objects.equals(timeStamp, that.timeStamp) && Objects.equals(information, that.information);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(year, month, day, hour, minute, action);
+        return Objects.hash(timeStamp, information);
     }
 
     @Override
     public String toString() {
         return "TimeStampInformation{" +
-                "year=" + year +
-                ", month=" + month +
-                ", day=" + day +
-                ", hour=" + hour +
-                ", minute=" + minute +
-                ", action='" + action + '\'' +
+                "timeStamp=" + timeStamp +
+                ", information='" + information + '\'' +
                 '}';
     }
 }
