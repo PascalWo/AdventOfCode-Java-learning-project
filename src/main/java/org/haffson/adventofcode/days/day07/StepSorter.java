8024c6153699ed21259f3fbd2d4b30c3d2990d70
PascalWo <pwolff.de@gmail.com>
2022-08-23 16:25:42 +0200

    deleted comments

--- src/main/java/org/haffson/adventofcode/days/day07/StepSorter.java
+++ src/main/java/org/haffson/adventofcode/days/day07/StepSorter.java
@@ -5,14 +5,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class StepSorter {
-
-    //    @Nonnull
-//    private final List<StepInstruction> stepInstructions;
-//    @Nonnull
-//    private List<StepInstruction> availableStepInstructions = new ArrayList<>();
-//    @Nonnull
-//    private List<StepInstruction> availableStepInstructionsNotUsed = new ArrayList<>();
-//    private StepInstruction nextStepInstruction;
     private final SortedSteps sortedSteps;
     private final SelectedSteps selectedSteps;
     private final Starter starter;
@@ -47,89 +39,6 @@ public class StepSorter {
         this.sortedSteps.addStep(starter.getStartingStep());
         this.selectedSteps.setNextStepToCheck(starter.getStartingStep());
     }
-//    public void setAvailableStarterOptions() {
-//        for (final char stepToCompare : startingSteps.getStartingSteps()) {
-//            selectedInstructions.availableStepInstructionsNotUsed.addAll(getAvailableStepInstructions(stepToCompare));
-//        }
-//    }
-
-//    private void setAvailableStepInstructions(final char stepToCompare) {
-//        final Set<StepInstruction> availableInstructions = new HashSet<>();
-//
-//        availableInstructions.addAll(getAvailableStepInstructions(stepToCompare));
-//        availableInstructions.addAll(availableStepInstructionsNotUsed);
-//
-//        this.availableStepInstructions = new ArrayList<>(availableInstructions);
-//    }
-
-//    @Nonnull
-//    private List<StepInstruction> getAvailableStepInstructions(final char stepToCompare) {
-//        return List.copyOf(stepInstructions).stream()
-//                .filter(stepInstruction -> stepToCompare == stepInstruction.step()).
-//                toList();
-//    }
-
-//    private void getNextStepInstructionByAlphabeticalOrder() {
-//        selectedInstructions.sortInstructionsAlphabeticalByFinishedBefore();
-//        for (int i = 0; i < selectedInstructions.getAmountOfAvailableStepInstructions(); i++) {
-//            if (isNextStepInstruction(i)) {
-//                setNextStepInstruction(i);
-//                setUnusedStepInstructions(i);
-//                break;
-//            }
-//        }
-//    }
-
-//    private void sortInstructionsAlphabeticalByFinishedBefore() {
-//        this.availableStepInstructions = availableStepInstructions
-//                .stream()
-//                .sorted(Comparator.comparing(StepInstruction::finishedBefore))
-//                .toList();
-//    }
-
-//    private boolean isNextStepInstruction(final int i) {
-//        final StepInstruction possibleNextInstruction = availableStepInstructions.get(i);
-//
-//        return isPrerequisiteComplete(possibleNextInstruction);
-//    }
-
-//    private boolean isPrerequisiteComplete(@Nonnull final StepInstruction instructionToCheck) {
-//        final char stepToCheck = instructionToCheck.finishedBefore();
-//
-//        if (stepIsAlreadyUsed(stepToCheck)) {
-//            return false;
-//        }
-//        final List<Character> stepsNeededToBeCompleted = getStepsNeedToBeCompleted(stepToCheck);
-//
-//        return isEveryNeededStepCompleted(stepsNeededToBeCompleted);
-//    }
-
-//    private boolean stepIsAlreadyUsed(final char stepToCheck) {
-//        return sortedSteps.getStepSequence().contains(stepToCheck);
-//    }
-
-//    @Nonnull
-//    private List<Character> getStepsNeedToBeCompleted(final char stepToCheck) {
-//        return List.copyOf(stepInstructions)
-//                .stream()
-//                .filter(stepInstruction -> stepToCheck == stepInstruction.finishedBefore())
-//                .map(StepInstruction::step)
-//                .toList();
-//    }
-
-//    private boolean isEveryNeededStepCompleted(@Nonnull final List<Character> stepsNeededToBeCompleted) {
-//        return new HashSet<>(sortedSteps.getStepSequence()).containsAll(stepsNeededToBeCompleted);
-//    }
-
-//    private void setNextStepInstruction(final int i) {
-//        this.nextStepInstruction = availableStepInstructions.get(i);
-//    }
-//
-//    private void setUnusedStepInstructions(final int i) {
-//        final List<StepInstruction> stepInstructionsNotUsed = new ArrayList<>(availableStepInstructions);
-//        stepInstructionsNotUsed.remove(i);
-//        this.availableStepInstructionsNotUsed = stepInstructionsNotUsed;
-//    }
 
     private void setNextStepToCheck() {
         if (selectedInstructions.getNextStepInstruction() != null) {
